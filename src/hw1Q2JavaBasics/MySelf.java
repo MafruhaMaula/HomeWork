26584886e44273b3022d0b01f0466c97e6538e81
mafruha maula <maulamafruha@gamil.com>
2022-01-16 17:31:00 -0500

    01/16/2022 5:30pm

---
+++ src/hw1Q2JavaBasics/MySelf.java
@@ -0,0 +1,17 @@
+package hw1Q2JavaBasics;
+
+/*
+ Multiple line comment:
+ Following is the main method and then printed it's value.
+ Naming convention:
+ Camel case example for this Project --> HomeWorkJava
+ Camel case example for a Package --> hw1Q2JavaBasics
+ Camel case example for a Class --> Myself
+*/
+
+public class MySelf {
+		public static void main(String[] args) {
+			System.out.println("My name is Mafruha Maula");
+		}
+
+}
---
+++ src/hw2Q2JavaBasics/MyCar.java
@@ -0,0 +1,25 @@
+package hw2Q2JavaBasics;
+ 
+
+public class MyCar {
+	
+	/*
+	 * (2) Create a package name "hw2Q2JavaBasics"[create it by the help of header, not by right click of src file] inside the Homework project created before. Create a Class "MyCar" from the header (not by right click of package, this is the other way to create class). Under main method the first outcome will be: [My name is <your name>, I love to drive, My care info is below:]. Use String concatenation feature from above outcome[10]. Now use other syso to print your car each info (color, model, year, wheel drive [example:4WD] etc.[you can add more if you want]) in a separate line [try to follow how to copy and paste a line in IDE by key board command]. Use print instead of println for the car feature when print[20], use \n and \t at least once anywhere in the class[10]. organize code at the end [10], After completing the code, paste it below.
+*
+	 */
+	
+	public static void main (String [] args) {
+		System.out.println("My name is Mafruha Maula." + "I love to drive."+ "My car info is below:");
+		System.out.print("\n\t1.Car Model: Toyota Rav4 2017.");
+		System.out.print("\n\t2.Color: Maroon.");
+		System.out. print("\n\t3.Pros: Loads of standard features and roomy seating and cargo space");
+		System.out. print("\n\t4.New for 2017: Toyota Safety Sense now standard in all trims");		
+		System.out.print("\n\t5.Best for: The 2017 Toyota RAV4's #12 ranking is based on its score within the 2017 Affordable Compact SUVs category. \n\tIt is the winner of our 2021 Best Used Cars for Teens award. Currently the Toyota RAV4 has a score of 7.7 out of 10, which is based on our evaluation of 41 pieces of research and data elements using various sources.");
+		
+		
+	}
+	
+	
+	
+
+}
