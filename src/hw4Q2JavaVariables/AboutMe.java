e0c8be042be0cecfcdb86f6a8c6e5e3860d3248e
mafruha maula <maulamafruha@gamil.com>
2022-01-24 16:20:08 -0500

    01242022 4:19

--- src/hw4Q2JavaVariables/AboutMe.java
+++ src/hw4Q2JavaVariables/AboutMe.java
@@ -22,7 +22,7 @@ public class AboutMe {
 	// method is implemented
 	public void aboutMe() {
 		System.out.println("My Name is: " + name + "\nMy Age: " + age + "\nYearlyHouseRent: " + myYearlyHouseRent
-				+ ", myYearlySalary" + myYearlySalary + "\nMy Bank Balance is: " + myBankBalance + "\nMy Height is: "
+				+ "\nmyYearlySalary:" + myYearlySalary + "\nMy Bank Balance is: " + myBankBalance + "\nMy Height is: "
 				+ myHeight + "\nMy Grade is: " + myGrade + "\nSex: " + sex + "\nAm I US Citizen? Ans: " + usCitizen);
 	}
 
