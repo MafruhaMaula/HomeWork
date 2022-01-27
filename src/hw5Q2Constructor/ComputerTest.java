31847242cd291837c03049fd6dba7e7e13a1fcbe
mafruha maula <maulamafruha@gamil.com>
2022-01-26 19:53:02 -0500

    01262022 7:53pm

--- src/hw5Q2Constructor/ComputerTest.java
+++ src/hw5Q2Constructor/ComputerTest.java
@@ -1,18 +1,13 @@
 package hw5Q2Constructor;
 
 public class ComputerTest {
-	
-	
+
 	public static void main(String[] args) {
-		
-		Computer computer1 = new Computer();
-		Computer computer2 = new Computer("Apple","MacBookAir", "MacOS Mojave", 800 ,  'A', false);
-		Computer computer3 = new Computer("Windows" , "Hp" , "Windows 10" , 1200 , 'A' , false);
-		
-		
-		
-		
-		
+
+		Computer computer1 = new Computer(); //Default constructor initialized
+		Computer computer2 = new Computer("Apple", "MacBookAir", "MacOS Mojave", 800, 'A', false); // Parameterized constructor initialized
+		Computer computer3 = new Computer("Windows", "Hp", "Windows 10", 1200, 'A', false);
+
 	}
 
 }
--- src/hw5Q3Constructor/StudentTest.java
+++ src/hw5Q3Constructor/StudentTest.java
@@ -1,20 +1,12 @@
 package hw5Q3Constructor;
 
-
-
 public class StudentTest {
-	
-	
-public static void main(String[] args) {
-		
-		Student student = new Student();
-		Student student1 = new Student( "Mafruha Maula", 124, 'F', true, 3.976f);
-	
-		
-			
-		
-		
-	}
 
+	public static void main(String[] args) {
+
+		Student student = new Student(); //Default constructor 
+		Student student1 = new Student("Mafruha Maula", 124, 'F', 3.976f, true);// Parameterized constructor initialized
+
+	}
 
 }
