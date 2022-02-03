adb55d340b76ccfa180f63e4865f139159a31e74
mafruha maula <maulamafruha@gamil.com>
2022-02-02 23:55:33 -0500

    02022022 11:55pm

--- src/hw6Q2IfElseCondition/Voter.java
+++ src/hw6Q2IfElseCondition/Voter.java
@@ -5,16 +5,15 @@ public class Voter {
 	public static void main(String[] args) {
 
 		int age = 18;
+
 		if (age == 18) {
-			System.out.println("I am a Voter");
+			System.out.println("I am a Voter new changes");
 		} else if (age < 18) {
-			System.out.println("I am not a Voter");
+			System.out.println("I am not a Voter new cahnges");
 		} else if (age >= 18) {
 			System.out.println("I am a Voter");
 		} else {
 			System.out.println("Please add a valid age");
 		}
-
 	}
-
 }
